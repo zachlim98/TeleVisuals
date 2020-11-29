eb5978ef4b915d572cf250c0f1f884722d1c1734
zachlim98 <zachlim98@users.noreply.github.com>
2020-11-29 18:13:29 +0800

    Updated required sources

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="TeleVisuals-pkg",
+    name="TeleVisuals",
     version="0.0.2",
     author="Zachary Lim",
     author_email="contact@nosparechange.com",
